66c5369ccacb3584b3dbaecd54b00f8f348fa901
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-02-11 16:55:50 -0500

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ $username = $_GET['username'];
 $password = $_GET['password'];
 $combo = $username;
 $combo .= $password;
-$LoginInfo = file_get_contents("/logins.txt");
+$LoginInfo = file_get_contents("logins.txt");
 echo $LoginInfo;
 if(strpos($LoginInfo, $combo) !== false)
 {
