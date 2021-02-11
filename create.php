97bab882e0cf8840716286f165351df570e4ff2d
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-02-11 17:59:13 -0500

    Update create.php

--- create.php
+++ create.php
@@ -7,7 +7,7 @@ fopen("logins.txt", "a");
 $file = fopen("logins.txt", "a");
 fwrite($file, $combo);
 //mkdir($combo);
-fopen($combo . "/index.php", "w");
+//fopen($combo . "/index.php", "w");
 fclose($combo . "/index.php");
 header("Location: index.html");
 ?>
