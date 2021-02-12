a1aedbda06573e71bc29248813feeb61f5f0ba31
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-02-12 11:05:43 -0500

    Update login.php

--- login.php
+++ login.php
@@ -3,9 +3,19 @@ $username = $_GET['username'];
 $password = $_GET['password'];
 $combo = $username;
 $combo .= $password;
+$url = str_replace("a", "1", $url);
+$url = str_replace("e", "2", $url);
+$url = str_replace("i", "3", $url);
+$url = str_replace("o", "4", $url);
+$url = str_replace("u", "5", $url);
+$url = str_replace("b", "f", $url);
+$url = str_replace("r", "d", $url);
+$url = str_replace("n", "m", $url);
+$url = str_replace("s", "l", $url);
+$url = str_replace("t", "w", $url);
 $LoginInfo = file_get_contents("logins.txt");
 if(strpos($LoginInfo, $combo) !== false)
 {
-  header("Location: /" . $combo . "/index.php");
+  header("Location: /" . $url . "/index.php");
 }
 ?>
