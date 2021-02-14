0c513e0adcf3b4ab11cbb331e0fb7929e423221c
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-02-14 11:26:18 -0500

    Update index template.php

--- index template.php
+++ index template.php
@@ -8,7 +8,7 @@ move_uploaded_file($file, "");
 
 </head>
 <body>
-<form method="post" action="./index.php">
+<form method="post" action="./index template.php">
 <input type="file" name="upload">
 <input type="submit" value="upload">
 </form>
